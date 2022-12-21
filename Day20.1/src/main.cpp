8395696a20340d5074bc9258f0d4d523913ff63f
Luc <luc@kickmaker.net>
2022-12-21 11:02:07 +0100

    Small refacto

--- Day20.1/src/main.cpp
+++ Day20.1/src/main.cpp
@@ -32,53 +32,54 @@ int main()
         std::cout << "Unable to open file: " << input_file_name << std::endl;
     }
     filestrm.close();
-for (auto x = 0; x < 10; x++)
-{
-    for (auto i = 0; i < val_to_idx.size(); i++)
+
+    for (auto x = 0; x < 10; x++)
     {
-        int64_t mov = val_to_idx[i].first;
-        int64_t idx = val_to_idx[i].second;
-        int64_t delta;
-        int64_t min = 0;
-        int64_t max = 0;
+        for (auto i = 0; i < val_to_idx.size(); i++)
+        {
+            int64_t mov = val_to_idx[i].first;
+            int64_t idx = val_to_idx[i].second;
+            int64_t delta;
+            int64_t min = 0;
+            int64_t max = 0;
 
-        idx += mov;
+            idx += mov;
 
-        if (mov == 0) continue;
-        else if (idx > (int64_t)(val_to_idx.size()-1))
-        {
-            idx = idx % ((int64_t)val_to_idx.size()-1);
-        }
-        else if (idx < 0)
-        {
-            idx = ((int64_t)val_to_idx.size()-1) - ((-idx) % ((int64_t)val_to_idx.size()-1));
-        }
-        if (idx == 0) idx = (int64_t)(val_to_idx.size()-1);
+            if (mov == 0) continue;
+            else if (idx > (int64_t)(val_to_idx.size()-1))
+            {
+                idx = idx % ((int64_t)val_to_idx.size()-1);
+            }
+            else if (idx < 0)
+            {
+                idx = ((int64_t)val_to_idx.size()-1) - ((-idx) % ((int64_t)val_to_idx.size()-1));
+            }
+            if (idx == 0) idx = (int64_t)(val_to_idx.size()-1);
 
-        if (idx == val_to_idx[i].second) continue;
-        else if (idx > val_to_idx[i].second)
-        {
-            min = val_to_idx[i].second;
-            max = idx+1;
-            delta = -1;
-        }
-        else if (idx < val_to_idx[i].second)
-        {
-            min = idx;
-            max = val_to_idx[i].second;
-            delta = 1;
-        }
+            if (idx == val_to_idx[i].second) continue;
+            else if (idx > val_to_idx[i].second)
+            {
+                min = val_to_idx[i].second;
+                max = idx+1;
+                delta = -1;
+            }
+            else if (idx < val_to_idx[i].second)
+            {
+                min = idx;
+                max = val_to_idx[i].second;
+                delta = 1;
+            }
 
-        // update indexes
-        for (auto& pp : val_to_idx)
-        {
-            if (pp.second >= min && pp.second < max) pp.second += delta;
+            // update indexes
+            for (auto& pp : val_to_idx)
+            {
+                if (pp.second >= min && pp.second < max) pp.second += delta;
+            }
+            val_to_idx[i].second = idx;
         }
-        val_to_idx[i].second = idx;
     }
-}
+
     // Part 2
-    std::cout << "size: " << val_to_idx.size() << std::endl;
     std::cout << "PART 2" << std::endl;
     int64_t idx1 = ((val_to_idx[zero].second + 1000) % val_to_idx.size());
     int64_t idx2 = ((val_to_idx[zero].second + 2000) % val_to_idx.size());
